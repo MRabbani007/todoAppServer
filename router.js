b96e9acb42fd37db69ec66516ad479f3496fb679
Mohamad Rabbani <mrabbani@outlook.com>
2024-02-01 19:26:25 +0500

    routes bug

--- router.js
+++ router.js
@@ -27,9 +27,9 @@ router.post("/", (req, res) => {
   res.json("Server Running");
 });
 
-// router.post("/:page", (req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send("Server Running");
-// });
+router.post("/:page", (req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Server Running");
+});
 
 module.exports = router;
